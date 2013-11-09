3eb7a4ffc196c638d38e3bdd8a3c89c76bbf9ece
Jeroen <jeroen@jeroens-mbp.lan>
2013-11-09 16:31:48 +0100

    Improved code thanks to http://www.reddit.com/r/webdev/comments/1q8t14/rate_my_thing_bootstrap_tree_view_with_checkboxes/cdad7pw

--- treeview.js
+++ treeview.js
@@ -28,33 +28,31 @@ $(document).ready(function () {
 	});
 
 	//When a checkbox is changed, its parents and children need to be updated.
-	$('.tree input[type=checkbox]').click(function () {
+	$('.tree input[type="checkbox"]').click(function () {
 	
-		checkbox_change_down(this);
-		checkbox_change_up(this);
+		checkboxChangeDown(this);
+		checkboxChangeUp(this);
 	});
 });
 
 //Returns the state of a checkbox out of {0,1,2} (2 being indeterminate)
-function get_checkbox_state(cb) {
+function getCheckboxState(cb) {
 	el = $(cb);
 
 	if (el.prop('indeterminate')) {
 		return 2;
-	} else {
-		if (el.prop('checked')) {
-			return 1;
-		} else {
-			return 0;
-		}
 	}
+	if (el.prop('checked')) {
+		return 1;
+	} 
+	return 0;	
 }
 
 //Sets the state of a checkbox out of {0,1,2}. Returns true if anything changed.
-function set_checkbox_state(el, new_state) {
+function setCheckboxState(el, new_state) {
 
 	//Determine current checkbox state
-	var old_state = get_checkbox_state(el);
+	var old_state = getCheckboxState(el);
 
 	if (new_state !== old_state) {
 		if (2 == new_state) {
@@ -69,77 +67,74 @@ function set_checkbox_state(el, new_state) {
 }
 
 //Determine new checkbox state due to change in children states
-function update_parent_cb(list_item) {
+function updateParentCheckbox(list_item) {
 
 	//Get this list-item's checkbox element
-	var checkbox = $(list_item).children('input[type=checkbox]:first');
+	var checkbox = $(list_item).children('input[type="checkbox"]:first');
 
 	//Obtain direct children
 	var child_items = $(list_item).children('ul').children('li')
-	var children = child_items.children('label').children('input[type=checkbox]');
-	var children = $.merge(children, child_items.children('input[type=checkbox]'));
+	var children = child_items.children('label').children('input[type="checkbox"]');
+	var children = $.merge(children, child_items.children('input[type="checkbox"]'));
 
 	//Determine new checkbox state to-be
 	var new_state = -1;
-	var done = false;
 	//Counters
 	var childrenon = 0;
 	var childrenoff = 0;
 	children.each(function (i, el) {
-		if (!done) {
-			//Determine checkbox state
-			var child_state = get_checkbox_state(el);
-			
-			if (2 == child_state) {
-				new_state = 2;
-				done = true;
+		//Determine checkbox state
+		var child_state = getCheckboxState(el);
+		
+		if (2 == child_state) {
+			new_state = 2;
+			return false; //break
+		} else {
+			if (child_state) {
+				childrenon++;
 			} else {
-				if (child_state) {
-					childrenon++;
-				} else {
-					childrenoff++;
-				}
-				if (childrenon > 0 && childrenoff > 0) {
-					new_state = 2;
-					done = true;
-				}
+				childrenoff++;
+			}
+			if (childrenon > 0 && childrenoff > 0) {
+				new_state = 2;
+				return false; //break
 			}
 		}
 	});
-	if (!done) {
+	if (new_state == -1) {
 		new_state = (childrenoff > 0) ? 0 : 1;
 	}
 
 	//Take action
-	if (set_checkbox_state(checkbox, new_state)) {
+	if (setCheckboxState(checkbox, new_state)) {
 		//This item's state has changed, so propagate to its parents as well
-		checkbox_change_up(checkbox.get(0));
+		checkboxChangeUp(checkbox.get(0));
 	}
 }
 
 //Update checkbox changes 'upwards' if necessary
-function checkbox_change_up(cb) {
+function checkboxChangeUp(cb) {
 	var parent2 = cb.parentElement.parentElement;
 
 	//Determine if this checkbox has parent-checkboxes
 	if ("LI" == parent2.tagName && "LI" == parent2.parentElement.parentElement.tagName) {
 
 		//cb is a normal child, and has a parent-checkbox which may need to change
-		update_parent_cb(parent2.parentElement.parentElement);
+		updateParentCheckbox(parent2.parentElement.parentElement);
 	} else if ("UL" == parent2.tagName && "LI" == parent2.parentElement.tagName) {
 	
 		//cb is a parent itself, and has a parent-checkbox which may need to change
-		update_parent_cb(parent2.parentElement);
+		updateParentCheckbox(parent2.parentElement);
 	}
 }
 
 //Update checkbox changes 'downwards' if necessary
-function checkbox_change_down(cb) {
+function checkboxChangeDown(cb) {
 	//Determine new state
-	var new_state = get_checkbox_state(cb);
+	var new_state = getCheckboxState(cb);
 
 	//Update any child-checkboxes
-	$(cb.parentElement).find('input[type=checkbox]').each(function (i, el) {
-		set_checkbox_state($(el), new_state);
+	$(cb.parentElement).find('input[type="checkbox"]').each(function (i, el) {
+		setCheckboxState($(el), new_state);
 	});
 }
